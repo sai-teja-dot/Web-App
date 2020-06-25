d3c20bd28a7a63bb9faad14b770f68404c317564
sai-teja-dot <60420350+sai-teja-dot@users.noreply.github.com>
2020-06-26 00:27:54 +0530

    Add files via upload

--- containers/DefaultLayout/DefaultFooter.js
+++ containers/DefaultLayout/DefaultFooter.js
@@ -15,8 +15,8 @@ class DefaultFooter extends Component {
 
     return (
       <React.Fragment>
-        <span><a href="http://aquagen.in/">AqauGen</a></span>
-        <span className="ml-auto">Powered by <a href="https://www.fluxgen.com/">FluxGen</a></span>
+        <span><a href="https://coreui.io">CoreUI</a> &copy; 2019 creativeLabs.</span>
+        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
       </React.Fragment>
     );
   }
